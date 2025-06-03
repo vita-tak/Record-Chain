d522694087af36151efb85406bf9d7d7aaa5cecf
IsacJulian <134926667+vita-tak@users.noreply.github.com>
2025-06-03 22:21:39 +0200

    Update records-controller.mjs

--- src/controllers/records-controller.mjs
+++ src/controllers/records-controller.mjs
@@ -1,7 +1,17 @@
 import RecordRepository from "../repositories/record-repository.mjs";
 
 export const listAllRecords = async (req, res) => {
-    
+
     const records = await new RecordRepository().listAll();
-    res.status(200).json({ success:true, statusCode: 200, data: records });
+    res.status(200).json({ success:true, statusCode: 200, data: records, message: "All records found successfully" });
+}
+
+export const findById = async (req, res) => {
+    const record = await new RecordRepository().findById(req.params.id);
+    res.status(200).json({ success: true, statusCode: 200, data: record, message: "Record found successfully" });
+}
+
+export const addRecord = async (req, res) => {
+    const record = await new RecordRepository().add(req.body);
+    res.status(201).json({ success: true, statusCode: 201, data: record, message: "Record added successfully" });
 }
