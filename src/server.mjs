6b721b7c4b0d51b07e19517f96696b5b1218c6b7
IsacJulian <134926667+vita-tak@users.noreply.github.com>
2025-06-03 22:16:21 +0200

    Update server.mjs

--- src/server.mjs
+++ src/server.mjs
@@ -3,7 +3,7 @@ import recordsRouter from "./routes/records-routes.mjs";
 
 const PORT = process.env.PORT || 4001;
 
-app.use("/api/v1/records", recordsRouter);
+app.use("/api/v1/records/", recordsRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on address http://localhost:${PORT} and is running in ${process.env.NODE_ENV} mode`);
