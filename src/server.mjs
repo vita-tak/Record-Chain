df9ee69821c6e2d4d6a90dde2301c6ea79edb12d
IsacJulian <134926667+vita-tak@users.noreply.github.com>
2025-06-04 16:31:26 +0200

    Update server.mjs

--- src/server.mjs
+++ src/server.mjs
@@ -1,9 +1,22 @@
 import { app } from "./app.mjs";
+import AppError from "./models/error/appError.mjs";
+import errorHandler from "./middleware/errorHandler.mjs";
 import recordsRouter from "./routes/records-routes.mjs";
 
 const PORT = process.env.PORT || 4001;
 
 app.use("/api/v1/records/", recordsRouter);
+
+app.all("*", (req, res, next) => {
+    next(
+        new AppError(
+            `Route "${req.originalUrl}" not found`,
+            404
+        )
+    );
+});
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server is running on address http://localhost:${PORT} and is running in ${process.env.NODE_ENV} mode`);
