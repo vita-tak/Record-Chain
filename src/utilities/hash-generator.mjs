401174276d14426d305046de065131bef4518638
IsacJulian <134926667+vita-tak@users.noreply.github.com>
2025-06-04 17:35:29 +0200

    Update hash-generator.mjs

--- src/utilities/hash-generator.mjs
+++ src/utilities/hash-generator.mjs
@@ -4,7 +4,9 @@ export const generateHash = (...args) => {
     const hash = crypto.createHash('sha256');
     hash.update(
         args
+        .map(arg => JSON.stringify(arg))
         .sort()
-        .join(''));
+        .join('')
+    );
     return hash.digest('hex');
 };
