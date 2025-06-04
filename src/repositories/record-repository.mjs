4811a93c1eda830f8c981ce3ab4e9b202fd77269
IsacJulian <134926667+vita-tak@users.noreply.github.com>
2025-06-04 17:51:27 +0200

    updates

--- src/repositories/record-repository.mjs
+++ src/repositories/record-repository.mjs
@@ -1,39 +1,39 @@
 export default class RecordRepository {
     
     async listAll() {
-        const response = await fetch("http://localhost:3000/records")
-        if (response.ok) {
-            const records = await response.json();
-            return records;
-        } else {
-            return response.status;
-        }
+        // const response = await fetch("http://localhost:3000/records")
+        // if (response.ok) {
+        //     const records = await response.json();
+        //     return records;
+        // } else {
+        //     return response.status;
+        // }
     }
 
     async findById (id) {
-        const response = await fetch(`http://localhost:3000/records/${id}`)
-        if (response.ok) {
-            const record = await response.json();
-            return record;
-        } else {
-            return response.status;
-        }
+        // const response = await fetch(`http://localhost:3000/records/${id}`)
+        // if (response.ok) {
+        //     const record = await response.json();
+        //     return record;
+        // } else {
+        //     return response.status;
+        // }
     }
 
     async add(record) {
-        const response = await fetch("http://localhost:3000/records", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(record)
-        })
+        // const response = await fetch("http://localhost:3000/records", {
+        //     method: "POST",
+        //     headers: {
+        //         "Content-Type": "application/json"
+        //     },
+        //     body: JSON.stringify(record)
+        // })
 
-        if (response) {
-            const record = await response.json();
-            return record;
-        } else {
-            return response.status;
-        }
+        // if (response) {
+        //     const record = await response.json();
+        //     return record;
+        // } else {
+        //     return response.status;
+        // }
     }
 }
--- src/server.mjs
+++ src/server.mjs
@@ -3,6 +3,9 @@ import AppError from "./models/error/appError.mjs";
 import errorHandler from "./middleware/errorHandler.mjs";
 import recordsRouter from "./routes/records-routes.mjs";
 
+import Blockchain from "./models/blockchain/Blockchain.mjs";
+export const blockchain = new Blockchain();
+
 const PORT = process.env.PORT || 4001;
 
 app.use("/api/v1/records/", recordsRouter);
