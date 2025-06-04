1edbd81435139fcdf37901b5d17970b540843f9a
IsacJulian <134926667+vita-tak@users.noreply.github.com>
2025-06-04 18:25:31 +0200

    Update record-repository.mjs

--- src/repositories/record-repository.mjs
+++ src/repositories/record-repository.mjs
@@ -6,50 +6,15 @@ export default class RecordRepository {
     
     async listAll() {
         return blockchain.chain;
-
-
-
-        // const response = await fetch("http://localhost:3000/records")
-        // if (response.ok) {
-        //     const records = await response.json();
-        //     return records;
-        // } else {
-        //     return response.status;
-        // }
     }
 
     async findById (id) {
         return blockchain.chain.find((block) => block.id === id);
-
-
-        // const response = await fetch(`http://localhost:3000/records/${id}`)
-        // if (response.ok) {
-        //     const record = await response.json();
-        //     return record;
-        // } else {
-        //     return response.status;
-        // }
     }
 
     async add(record) {
         record.id = crypto.randomUUID().replaceAll("-", "");
         blockchain.addBlock({ data: record });
         return blockchain.chain;
-
-
-        // const response = await fetch("http://localhost:3000/records", {
-        //     method: "POST",
-        //     headers: {
-        //         "Content-Type": "application/json"
-        //     },
-        //     body: JSON.stringify(record)
-        // })
-
-        // if (response) {
-        //     const record = await response.json();
-        //     return record;
-        // } else {
-        //     return response.status;
-        // }
     }
 }
