63302adc3667d5a63e6472e6e5ef58ec8b14a7b3
IsacJulian <134926667+vita-tak@users.noreply.github.com>
2025-06-05 19:29:06 +0200

    Update records-controller.mjs

--- src/controllers/records-controller.mjs
+++ src/controllers/records-controller.mjs
@@ -1,8 +1,14 @@
 import RecordRepository from "../repositories/record-repository.mjs";
+import JsonFileStorage from "../JsonFileStorage.mjs";
 import { catchErrorAsync } from "../utilities/catchErrorAsync.mjs";
+
+const recordStorage = new JsonFileStorage('data', 'records.json');
 
 export const listAllRecords = catchErrorAsync(async (req, res) => {
     const records = await new RecordRepository().listAll();
+
+    await recordStorage.writeToFile(JSON.stringify(records, null, 2));
+
     res.status(200).json({ success:true, statusCode: 200, blocks: records});
 });
 
@@ -13,5 +19,9 @@ export const findById = catchErrorAsync(async (req, res) => {
 
 export const addRecord = catchErrorAsync(async (req, res) => {
     const record = await new RecordRepository().add(req.body);
+
+    const allRecords = await new RecordRepository().listAll();
+    await recordStorage.writeToFile(JSON.stringify(allRecords, null, 2));
+
     res.status(201).json({ success: true, statusCode: 201, block: record});
 });
