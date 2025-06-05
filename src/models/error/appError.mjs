9cd64059cd248489718082dd3e4c6cd853a3debc
IsacJulian <134926667+vita-tak@users.noreply.github.com>
2025-06-05 15:16:00 +0200

    Update appError.mjs

--- src/models/error/appError.mjs
+++ src/models/error/appError.mjs
@@ -1,35 +1,34 @@
 export default class AppError extends Error {
-    constructor(message, statusCode) {
-      super(message);
-      this.statusCode = statusCode;
-  
-      switch (statusCode) {
-        case 400:
-          this.status = 'Bad Request, information saknas';
-          break;
-        case 401:
-          this.status = 'Unauthorized, du måste vara inloggad';
-          break;
-        case 403:
-          this.status = 'Unauthorized, du har inte behörighet';
-          break;
-        case 404:
-          this.status = 'Not Found, vi hittar inte resursen som du frågar efter';
-          break;
-        case 405:
-          this.status = 'Metod som används är inte tillåten';
-          break;
-        case 415:
-          this.status = 'Media typen stöds inte';
-          break;
-        case statusCode.toString().startsWith('5'):
-          this.status = 'Internal Server Error';
-          break;
-        default:
-          this.status = 'Det gick fel, vet inte vad';
-      }
-  
-      Error.captureStackTrace(this, this.constructor);
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+
+    switch (statusCode) {
+      case 400:
+        this.status = 'Bad Request, information is missing';
+        break;
+      case 401:
+        this.status = 'Unauthorized, you must be logged in';
+        break;
+      case 403:
+        this.status = 'Forbidden, you do not have permission';
+        break;
+      case 404:
+        this.status = 'Not Found, we cannot locate the requested resource';
+        break;
+      case 405:
+        this.status = 'Method Not Allowed';
+        break;
+      case 415:
+        this.status = 'Unsupported Media Type';
+        break;
+      case statusCode.toString().startsWith('5'):
+        this.status = 'Internal Server Error';
+        break;
+      default:
+        this.status = 'Something went wrong, unknown error';
     }
+
+    Error.captureStackTrace(this, this.constructor);
   }
-  
+}
