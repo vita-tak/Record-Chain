0383334d1155a153a9a824b299f87bbe8d41c2a2
IsacJulian <134926667+vita-tak@users.noreply.github.com>
2025-06-05 15:53:21 +0200

    Update record-repository.mjs

--- src/repositories/record-repository.mjs
+++ src/repositories/record-repository.mjs
@@ -6,18 +6,22 @@ import AppError from "../models/error/appError.mjs";
 export default class RecordRepository {
     
     async listAll() {
-
         return blockchain.chain;
     }
 
     async findById (id) {
-        return blockchain.chain.find((block) => block.id === id);
+        const record = blockchain.chain.find((block) => block.id === id);
+
+        if(!record) 
+            throw new AppError(`Did not find block with id: '${id}'`, 404);
+        return record;
     }
 
     async add(record) {
-        const {title, artist, genre, formats} = record;
+        const {title, artist} = record;
 
         if (!title) throw new AppError("Title is required", 400);
+        if (!artist) throw new AppError("Artist is required", 400);
 
         record.id = crypto.randomUUID().replaceAll("-", "");
         blockchain.addBlock({ data: record });
